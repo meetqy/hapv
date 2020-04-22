0ff3cedfe5af7c9a048b1c317e77f7c1acf85740
meetqy <meetqy@qq.com>
2020-04-22 13:53:41 +0800

    播放记录store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import playLog from "./play_log";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    playLog,
+  },
 });
---
+++ src/store/play_log.js
@@ -0,0 +1,19 @@
+// 播放记录
+const playLog = {
+  state: {
+    data: [
+      /*
+      {
+        url: "", // 视频地址
+        date: "", // 播放时间，最后一次关闭时间
+        videoTime: "", // 视频播放到哪里
+      },
+      */
+    ],
+  },
+  mutations: {},
+  actions: {},
+  getters: {},
+};
+
+export default playLog;
