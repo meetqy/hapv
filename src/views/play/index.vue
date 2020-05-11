f7b13ad6e1427ed57a1f91b45435235cc1de4aa3
meetqy <meetqy@qq.com>
2020-05-11 11:13:00 +0800

    增加下载按钮

--- src/views/play/index.vue
+++ src/views/play/index.vue
@@ -3,6 +3,11 @@
     <Header>
       <template v-slot:left>
         <el-button @click="drawer = !drawer" size="mini">播放记录</el-button>
+        <div style="margin-left: 20px">
+          <el-button type="primary" icon="el-icon-plus" size="mini">
+            加入下载
+          </el-button>
+        </div>
       </template>
       <template v-slot:right>
         <el-select
